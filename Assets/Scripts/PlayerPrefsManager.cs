37235306b043b89d483d64dde9bd4812ed77c256
Arthur Dvurechenskiyi <dvurechenskiyi@cdc.ru>
2017-08-30 19:10:31 +0300

    Added PlayerPrefsManager

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    public const string MASTER_VOLUME_KEY = "MASTER_VOLUME_KEY";
+    public const string DIFFICULTY_KEY = "DIFFICULTY_KEY";
+    public const string LEVEL_KEY = "LEVEL_KEY";
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
